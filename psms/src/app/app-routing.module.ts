c41a9163335b4eb51799e3c416464dd6f75f79af
Peter Mok <43172966+petermok-1213@users.noreply.github.com>
2022-10-10 21:35:43 +1100

    UPDATED imports; ADDED angular material

--- psms/src/app/app-routing.module.ts
+++ psms/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CheckStockComponent } from './components/check-stock/check-stock.component';
+import { HomeComponent } from './components/home/home.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { ReviewComponent } from './components/review/review.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "home", component: HomeComponent },
+  { path: "checkStock", component: CheckStockComponent },
+  { path: "review", component: ReviewComponent },
+  { path: "notFound", component: NotFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- psms/src/app/app.module.ts
+++ psms/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { CheckStockComponent } from './components/check-stock/check-stock.component';
+import { ReviewComponent } from './components/review/review.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CheckStockComponent,
+    ReviewComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,

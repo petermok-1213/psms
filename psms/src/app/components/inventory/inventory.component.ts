5760be9b7e240c845d1e424117a95f38b7bef85f
Peter Mok <43172966+petermok-1213@users.noreply.github.com>
2022-11-11 17:42:41 +1100

    ADDED filters opeartion in inventory

--- psms/src/app/components/inventory/inventory.component.ts
+++ psms/src/app/components/inventory/inventory.component.ts
@@ -25,17 +25,68 @@ export class InventoryComponent implements OnInit {
     }
   ]
 
-  onClickMinus(item: any) {
-    item.qty -= 1
-  }
-
-  onClickPlus(item: any) {
-    item.qty += 1
-  }
+  tagSet = new Set()
+  filterDict = new Map()
+  outList = this.tempDb;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.tempDb.forEach((item) => {
+      this.tagSet.add(item.tag)
+    })
+    this.tagSet.forEach((tag) => {
+      this.filterDict.set(tag, false)
+    })
   }
+
+  // Called upon + button of each card is clicked
+  onClickMinus(item: any) {
+    item.qty -= 1
+  }
+
+  // Called upon - button of each card is clicked
+  onClickPlus(item: any) {
+    item.qty += 1
+  }
+
+  // Filter tempDb based on the values of each tag in tagDict
+  filterList() {
+    if (!this.hasFilter())                    // No filter, return whole list
+      this.outList = this.tempDb
+    else {                                    // Return a filtered list
+      this.outList = new Array()
+      this.tempDb.forEach((item) => {
+      if (this.filterDict.get(item.tag)) {
+          this.outList.push(item)
+        }
+      })   
+    }
+  }
+
+  // Called upon selection of checkbox
+  onSelectionChange(tag: any) {
+    this.filterDict.set(tag, !this.filterDict.get(tag))   // toggle filter
+    this.filterList()
+  }
+
+  // For "All" radio, return true if there is at least 1 filter, false otherwise
+  hasFilter() {
+    let filter = false
+    this.filterDict.forEach((value) => {
+      filter = filter || value
+    })
+    return filter
+  }
+
+  // Called upon "ClearFilterButton" is clicked, removes all filters
+  clearFilter() {
+    for (let key of this.filterDict.keys()) {
+      this.filterDict.set(key, false)
+    }
+    this.filterList()
+  }
+
+
 
 }

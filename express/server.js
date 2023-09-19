71b56a1fd8df7184db41d3bbb7dd699521289090
petermok-1213 <petermok1213@gmail.com>
2023-09-19 19:00:34 +1000

    ADDED MongoDb connection

--- express/server.js
+++ express/server.js
@@ -1,11 +1,25 @@
 const express = require('express')
 const app = express()
+const { MongoClient } = require('mongodb')
 
 if (process.env.NODE_ENV !== 'production') {
     require('dotenv').config()
 }
 
 const PORT = process.env.PORT || 8888
+const mongoClient = new MongoClient(process.env.MONGO_URI)
+
+async function connectToDb(mongoClient) {
+    await mongoClient.connect().catch((err) => {
+        console.log(err+" in connecting to Db")
+        return null
+    })
+    if (mongoClient) {
+        console.log("Connected to MongoDb")
+    }
+}
+
+connectToDb(mongoClient)
 
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", 
@@ -29,3 +43,4 @@ app.post('/prompt', (req, res) => {
     console.log('received prompt')
     res.json({})
 })
+

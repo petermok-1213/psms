3851d67eb4c63b4a998790c4e0c06ab85d9237ed
petermok-1213 <petermok1213@gmail.com>
2023-09-27 16:56:18 +1000

    REFACTORED Item into Product

---
+++ psms/src/app/classes/Product.ts
@@ -0,0 +1,17 @@
+/*
+    A generic Product class representing all products stored in the inventory
+*/
+class Product {
+    name: String
+    tag: String
+    price: number
+    quantity: number
+
+    constructor(name: String, tag: String, quantity: number, price: number) {
+        this.name = name
+        this.tag = tag
+        this.price = price
+        this.quantity = quantity
+    }
+
+}
